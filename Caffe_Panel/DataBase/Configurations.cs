a8abb0a9ad591d6624493f9238fae805e9c14ac8
Daniel Ostapenkov <84470259+DanOstap@users.noreply.github.com>
2024-08-15 02:55:15 +0200

    Create User Servuce

--- Caffe_Panel/DataBase/Configurations.cs
+++ Caffe_Panel/DataBase/Configurations.cs
@@ -1,6 +1,23 @@
-﻿namespace Caffe_Panel.DataBase
+﻿using Caffe_Panel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caffe_Panel.DataBase
 {
-    public class Configurations
+    public class Configurations: DbContext
     {
+     public  DbSet<User> User { get; set; }
+     public DbSet<Items> Items { get; set; }
+
+        public Configurations(DbContextOptions<Configurations> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasKey("Id");
+            modelBuilder.Entity<Items>().HasKey("Id");
+
+        }
     }
 }
---
+++ Caffe_Panel/DataBase/Service/UserService.cs
@@ -0,0 +1,76 @@
+﻿using Caffe_Panel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caffe_Panel.DataBase.Service
+{
+    public interface IUsersService
+    {
+        public Task<User> CreateUser(User model);
+        public Task<List<User>?> FindAll();
+        public Task<User?> FindOneById(int id);
+        public Task<User?> FindOneByName(string name);
+        public Task<User?> Update(int id, User model);
+        public Task<User?> RemoveById(int id);
+        public Task<User?> RemoveByName(string name);
+    }
+
+    public class UserService : IUsersService
+    {
+       private readonly Configurations _configuration;
+        public UserService(Configurations configuration)
+        {
+            _configuration = configuration;
+        }
+        async public Task<User> CreateUser(User model) {
+           var user = await _configuration.User.AddAsync(model);
+            await _configuration.SaveChangesAsync();
+            return model;
+        }
+        async public Task<List<User>?> FindAll() {
+            if (_configuration.User == null) return null;
+            return await _configuration.User.ToListAsync();
+        }
+        async public Task<User?> FindOneById(int id) {
+            var user = await _configuration.User.FindAsync(id);
+            if (user != null) return user;
+            return null;
+        }
+        async public Task<User?> FindOneByName(string name) {
+            var user = await _configuration.User.Where(i => i.Name_Subname == name).FirstOrDefaultAsync();
+            if (user != null) return user;
+            return null;
+        }
+        async public Task<User?> Update(int id, User model) {
+            var user = await _configuration.User.FindAsync(id);
+            if (user != null) {
+                _configuration.Entry(model).State = EntityState.Modified;
+                try {
+                    await _configuration.SaveChangesAsync();
+                    return model;
+                }
+                catch (DbUpdateConcurrencyException err) {
+                    Console.WriteLine(err);
+                    return null;
+                }
+           }
+            return null;
+        }
+        async public  Task<User?> RemoveById(int id) {
+            var user = await _configuration.User.FindAsync(id);
+            if (user != null) {
+                _configuration.User.Remove(user);
+                return user;
+            }
+            return null;
+        }
+        async public Task<User?> RemoveByName(string name) {
+            var user = await _configuration.User.Where(i => i.Name_Subname == name).FirstOrDefaultAsync();
+            if (user != null)
+            {
+                _configuration.User.Remove(user);
+                return user;
+            }
+            return null;
+        }
+    }
+}
